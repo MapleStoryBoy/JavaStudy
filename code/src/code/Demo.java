7a6c47253a0f151fe4e072f61d3095fe4afb9d82
MapleStoryBoy <MapleStoryBoy@163.com>
2019-05-19 19:45:50 +0800

    Java基础篇

---
+++ code/src/code/Demo.java
@@ -0,0 +1,27 @@
+package code;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 	* 字符串常量	用双引号括起来的内容
+		 	* 整数常量		所有整数
+		 	* 小数常量		所有小数
+		 	* 字符常量		用单引号括起来的内容,里面只能放单个数字,单个字母或单个符号
+		 	* 布尔常量		较为特殊，只有true和false
+		 	* 空常量		    null(数组部分讲解)
+		 */
+		System.out.println("Hello world!");
+		System.out.println("abc");
+		System.out.println(123);
+		System.out.println(12.3);
+		//System.out.println('10');
+		//System.out.println('');
+		System.out.println(' ');
+		System.out.println(true);
+		System.out.println(false);
+
+	}
+
+}
