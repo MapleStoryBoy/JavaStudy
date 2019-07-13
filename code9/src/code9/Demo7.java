214e7d2ba40c6451af0040e6d1ff4c77b6bce2f2
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-13 21:03:27 +0800

    JavaStudy

---
+++ code9/src/code9/Demo7.java
@@ -0,0 +1,19 @@
+package code9;
+
+public class Demo7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 强制类型转换
+		double x = 3.14;
+		int nx = (int)x;
+		char c = 'a';
+		int d = c + 1;
+		System.out.println(nx);
+		System.out.println(d);
+		System.out.println((char)d);
+
+	}
+
+}
