14550f0c840045022062d1771825ffd56c36c299
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-13 22:35:39 +0800

    JavaStudy

---
+++ code9/src/code9/TestSwitch2.java
@@ -0,0 +1,29 @@
+package code9;
+
+public class TestSwitch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c = 'a';
+		int rand =(int) (26*Math.random()); 
+		char c2 = (char)(c+rand); 
+		System.out.print(c2 + ": ");
+		switch (c2) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			System.out.println("元音");
+			break; 
+		case 'y':
+		case 'w':
+			System.out.println("半元音");
+			break;
+		default:
+		System.out.println("辅音");
+		}
+
+	}
+
+}
