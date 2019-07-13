4c0efe85d55e9cd4d087e2c36c9f5a9b8bd8b987
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-13 21:56:01 +0800

    JavaStudy

---
+++ code10/src/Demo1.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * author:Jsp
+ */
+
+
+public class Demo1 {
+    public static void main(String[] args) {
+        // 流程控制语句----if语句
+        double d = Math.random();
+        System.out.println(d);
+        //System.out.println((int)(6*Math.random()+1));
+
+        int i = (int)(6*Math.random()+1);
+        System.out.println(i);
+
+        if (i<=3){
+            System.out.println("小");
+            System.out.println("小小小");
+        }
+    }
+
+}
