855084a15fe35abaeff40f6c762c07e8713dbda4
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-13 19:11:07 +0800

    JavaStudy

---
+++ code9/src/code9/Demo4.java
@@ -0,0 +1,23 @@
+package code9;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 关系运算符
+		int a = 3;
+		System.out.println(a==3);
+		System.out.println(a!=3);
+		System.out.println(a<5);
+		
+		char b = 'a';
+		char b2 = 'c';
+		System.out.println((int)b);
+		System.out.println(0+b);
+		System.out.println(0+b2);
+		System.out.println(b<b2);
+
+	}
+
+}
