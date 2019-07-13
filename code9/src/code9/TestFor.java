9d70f7e2cf4c92001f9b3baf0919184ccb72cb21
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-13 23:12:52 +0800

    JavaStudy

---
+++ code9/src/code9/TestFor.java
@@ -0,0 +1,23 @@
+package code9;
+
+public class TestFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//用while和for循环分别计算100以内奇数和偶数的和，并输出
+		int sum1 = 0;
+		int sum2 = 0;
+		
+		for (int i = 1;i<100;i++) {
+			if (i%2==0) {
+				sum1 += i;
+			}else {
+				sum2 += i;
+			}
+		}
+		System.out.println(sum1);
+		System.out.println(sum2);
+
+	}
+
+}
