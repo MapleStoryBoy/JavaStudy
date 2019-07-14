f0ef0eb64908b86199968ec2c2cf1b853e8caf95
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-14 13:18:04 +0800

    JavaStudy

--- code9/src/code9/TestContinue.java
+++ code9/src/code9/TestContinue.java
@@ -4,7 +4,7 @@ public class TestContinue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//把100～150之间不能被3整除的数输出。
+		//把100～150之间不能被3整除的数输出。每行输出5个
 		int count = 0;//定义计数器
 		for (int i=100;i<150;i++) {
 			if (i%3==0) {
@@ -16,7 +16,5 @@ public class TestContinue {
 				System.out.println();
 			}
 		}
-
 	}
-
 }
