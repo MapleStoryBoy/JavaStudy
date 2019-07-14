032680d0733eba711bc9ec7fa96af363f6d7565e
MapleStoryBoy <MapleStoryBoy@163.com>
2019-07-14 18:18:14 +0800

    JavaStudy

---
+++ code12/src/code12/TestConstructor.java
@@ -0,0 +1,25 @@
+package code12;
+
+public class TestConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p = new Point(3.0,4.0);
+		Point origin = new Point(0.0,0.0);
+		System.out.println(p.getDistance(origin));
+	}
+
+}
+class Point {
+	double x,y;
+	//构造器的方法名和类名必须保持一致
+	public Point(double _x,double _y) {
+		x = _x;
+		y = _y;
+	}
+	
+	public double getDistance(Point p) {
+		return Math.sqrt((x-p.x)*(x-p.x) + (y-p.y)*(y-p.y));
+		
+	}
+}
